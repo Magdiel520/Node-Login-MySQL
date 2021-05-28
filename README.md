573b4c760cf9bb7e9a529000760223b8b3a6c934
Magdiel Rotta Almeida <36762111+Magdiel520@users.noreply.github.com>
2021-05-28 14:01:58 -0300

    First upload
    Hope you enjoy!

---
+++ README.md
@@ -0,0 +1,10 @@
+HI
+
+This is a pattern NodeJS Backend application you may use to login and MySQL
+database.
+
+I hope you enjoy!
+
+Best Regards,
+
+***Dev Tiger Al-Namur***
---
+++ database.js
@@ -0,0 +1,12 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        // You may configure your connection to DB, by changing the pattern datas.
+        'host': '127.0.0.1',
+        'user': 'root',
+        'password': 'password',
+        'database': 'db'
+    },
+    'database': 'db',
+
+};
